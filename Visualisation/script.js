e3fe337ad8b9105b47ade7eff54815a0374d3cb8
henrythier <henry@thierhoff.rocks>
2020-06-08 16:23:16 +0200

    added visualisation

--- Visualisation/script.js
+++ Visualisation/script.js
@@ -1,18 +1,43 @@
-var TITLE = 'Hartford School District is No Longer the Largest in Connecticut';
+var TITLE = 'Bi-Monthly Polls Data for Major German Parties';
 
-var X_AXIS = 'Academic Year';  // x-axis label and label in tooltip
-var Y_AXIS = 'Number of Students'; // y-axis label and label in tooltip
+var X_AXIS = 'Date';  // x-axis label and label in tooltip
+var Y_AXIS = 'Poll'; // y-axis label and label in tooltip
 
 var BEGIN_AT_ZERO = false;  // Should x-axis start from 0? `true` or `false`
 
 var SHOW_GRID = true; // `true` to show the grid, `false` to hide
 var SHOW_LEGEND = true; // `true` to show the legend, `false` to hide
+var party_colors = {
+    'CDU/CSU': [0, 0, 0],
+    'SPD': [227, 0, 15],
+    'GRÜNE': [100, 161, 45],
+    'FDP': [255,237,0],
+    'LINKE': [165,36,96],
+    'AfD': [0,102,153],
+    'REP': [139,69,19],
+    'PIRATEN': [255,136,0],
+    'Rechte': [139,69,19],
+    'Non-Voters': [211,211,211],
+    'FW': [0,126,132],
+    'NPD': [139,69,19],
+    'KPD/DKP': [255,0,0],
+    'DRP': [105,105,105],
+    'GB/BHE': [125,125,125],
+    'DP': [145,145,145],
+    'BP': [165,165,165],
+    'Zentrum': [185,185,185],
+    'Other': [85,85,85]
+  }
 
+function to_rgba(rgb, opacity) {
+  str = 'rgba(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ',' + opacity + ')'
+  return str;   // The function returns the product of p1 and p2
+}
 
 $(document).ready(function() {
 
   // Read data file and create a chart
-  $.get('https://raw.githubusercontent.com/HandsOnDataViz/chartjs-templates/master/line-chart/data.csv', function(csvString) {
+  $.get('https://raw.githubusercontent.com/henrythier/polls/master/Data/normalised/semi_month.csv', function(csvString) {
 
     var data = Papa.parse(csvString).data;
     var timeLabels = data.slice(1).map(function(row) { return row[0]; });
@@ -23,7 +48,13 @@ $(document).ready(function() {
         {
           label: data[0][i], // column name
           data: data.slice(1).map(function(row) {return row[i]}), // data in that column
-          fill: false // `true` for area charts, `false` for regular line charts
+          fill: false, // `true` for area charts, `false` for regular line charts
+          borderColor: to_rgba(party_colors[data[0][i]], 0.8),
+          backgroundColor: to_rgba(party_colors[data[0][i]], 0.5),
+          pointBackgroundColor: to_rgba(party_colors[data[0][i]], 0.5),
+          pointBorderColor: to_rgba(party_colors[data[0][i]], 1),
+          pointHoverBackgroundColor: to_rgba(party_colors[data[0][i]], 0.8),
+          pointHoverBorderColor: to_rgba(party_colors[data[0][i]], 1),
         }
       )
     }
